b126f10bb5214bd5042a05209b2b27b357bf6327
masai <varshakumari8999@gmail.com>
2021-09-08 11:55:11 +0530

    sms read

--- Crip/app/src/main/java/com/varsha/cripassignment/MainActivity.kt
+++ Crip/app/src/main/java/com/varsha/cripassignment/MainActivity.kt
@@ -1,40 +1,38 @@
 package com.varsha.cripassignment
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val listView = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mSmsinboxQueryUri = Uri.parse("content://sms/inbox")
-        val cursor1 = contentResolver.query(
-            mSmsinboxQueryUri,
-            arrayOf("_id", "thread_id", "address", "person", "date", "body", "type"),
-            null,
-            null,
-            null
-        )
-        startManagingCursor(cursor1)
-        val columns = arrayOf("address", "person", "date", "body", "type")
-        if (cursor1!!.count > 0) {
-            val count = Integer.toString(cursor1.count)
-            while (cursor1.moveToNext()) {
-                val address = cursor1.getString(cursor1.getColumnIndex(columns[0]))
-                if (address.equals("+917091434563", ignoreCase = true)) { //put your number here
-                    val name = cursor1.getString(cursor1.getColumnIndex(columns[1]))
-                    val date = cursor1.getString(cursor1.getColumnIndex(columns[2]))
-                    val body = cursor1.getString(cursor1.getColumnIndex(columns[3]))
-                    val type = cursor1.getString(cursor1.getColumnIndex(columns[4]))
-                    Log.d("***", "body=$body")
-                    testSms.text=body
-                }
-            }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_SMS), 0)
         }
+        else{
+            val uri = Uri.parse("content://sms/inbox")
+            val cursor = contentResolver.query(uri, null, null, null, null)
+             while (cursor!!.moveToNext() == true){
+                 val PhoneMessage = cursor.getString(12)
+                 val PhoneNumber = cursor.getString(2)
+                 listView.add("$PhoneNumber \n $PhoneMessage")
+             }
+        }
+        val listAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_2,listView)
+        list_view.adapter = listAdapter
     }
 }
 
